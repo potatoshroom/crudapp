40f447a5203b1a3ca663a70ffbc84a9da3355444
potatoshroom <francecabiles@gmail.com>
2023-10-14 01:17:09 +0800

    First Commit!

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use App\Models\Task;
+
+class TaskController extends Controller
+{
+    public function index()
+    {
+        $tasks = Task::all();
+        return view('tasks.index', ['tasks' => $tasks]);
+    }
+
+    public function create()
+    {
+        return view('tasks.create');
+    }
+
+    public function store(Request $request)
+    {
+        Task::create($request->all());
+        return redirect()->route('tasks.index');
+    }
+
+    public function show(Task $task)
+{
+    return view('tasks.show', compact('task'));
+}
+
+
+    public function edit(Task $task)
+    {
+        return view('tasks.edit', compact('task'));
+    }
+
+    public function update(Request $request, Task $task)
+    {
+        $task->update($request->all());
+        return redirect()->route('tasks.index');
+    }
+
+    public function destroy(Task $task)
+    {
+        $task->delete();
+        return redirect()->route('tasks.index');
+    }
+}
